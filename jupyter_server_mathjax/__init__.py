c01f7f8ad710b070a4238a7dfc21534a173b533b
Vidar Tonaas Fauske <vidartf@gmail.com>
2021-01-08 14:35:28 +0100

    initial working condition

--- jupyter_server_mathjax/__init__.py
+++ jupyter_server_mathjax/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from jupyter_server.base.handlers import FileFindHandler
+from jupyter_server.utils import url_path_join
 
 STATIC_ASSETS_PATH = os.path.join(os.path.dirname(__file__), 'static')
 
@@ -12,11 +13,10 @@ def _jupyter_server_extension_points():
     ]
 
 def _load_jupyter_server_extension(serverapp):
-    static_url_prefix = serverapp.static_url_prefix
+    static_url_prefix = serverapp.web_app.settings['static_url_prefix']
 
-    serverapp.handlers.append(
-        (static_url_prefix + r"/components/MathJax/(.*)", FileFindHandler, {
+    serverapp.web_app.add_handlers('.*$', [
+        (url_path_join(static_url_prefix, r"components/MathJax/(.*)"), FileFindHandler, {
             'path': STATIC_ASSETS_PATH,
         })
-    )
-
+    ])
