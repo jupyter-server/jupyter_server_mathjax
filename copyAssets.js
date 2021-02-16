90a9b127954dd829abb3ef5066c88c6658cb4c0d
Vidar Tonaas Fauske <vidartf@gmail.com>
2021-02-16 16:40:48 +0000

    trim mathjax assets

--- copyAssets.js
+++ copyAssets.js
@@ -1,56 +1,90 @@
-const fse = require('fs-extra')
-const path = require('path');
+const fse = require("fs-extra");
+const path = require("path");
 
-const srcDir = path.join(__dirname, 'node_modules', 'mathjax');
-const dstDir = path.join(__dirname, 'jupyter_server_mathjax', 'static');
-// Exclude unneeded files:
-const exclude = ['unpacked', 'test', 'package.json', 'README.md', 'CONTRIBUTING.md', 'latest.js'];
+const srcDir = path.join(__dirname, "node_modules", "mathjax");
+const dstDir = path.join(__dirname, "jupyter_server_mathjax", "static");
 
-function inSubDir(candidate, root) {
-  const relative = path.relative(root, path.dirname(candidate));
-  return relative && !relative.startsWith('..') && !path.isAbsolute(relative);
+/*
+ * Copy MathJax static assets, but trim which assets in a similar way to what
+ * notebook does on dist (in setupbase).
+ */
+
+const rps = path.sep.replace("\\", "\\\\");
+
+function re_join(parts) {
+  return parts.join(rps);
+}
+
+const include = [
+  ["MathJax.js"],
+  ["LICENSE"],
+  ["config", "TeX-AMS-MML_HTMLorMML-full.js"],
+  ["config", "Safe.js"],
+];
+
+const re_include = [
+  ["jax", "output", `[^${rps}]+.js$`],
+  ["jax", "output", "autoload", ".*"],
+  ["localization", ".*"],
+  ["fonts", "HTML-CSS", "STIX-Web", "woff", ".*"],
+  ["extensions", ".*"],
+  ["jax", "input", "TeX", ".*"],
+  ["jax", "output", "HTML-CSS", "fonts", "STIX-Web", ".*"],
+  ["jax", "output", "SVG", "fonts", "STIX-Web", ".*"],
+  ["jax", "element", "mml", ".*"],
+];
+
+function isPartial(parts, candidate) {
+  return parts.length <= candidate.length;
+}
+
+function partialPathMatch(parts, candidate) {
+  const np = Math.min(candidate.length, parts.length);
+  for (let i = 0; i < np; ++i) {
+    if (candidate[i] !== parts[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function pathOk(p) {
+  if (!p) {
+    return true;
+  }
+  const parts = p.split(path.sep);
+  for (let c of include) {
+    if (isPartial(parts, c)) {
+      // Check for partial matches (to ensure dirs get included)
+      if (partialPathMatch(parts, c)) {
+        return true;
+      }
+    } else {
+      if (c.join(path.sep) == p) {
+        return true;
+      }
+    }
+  }
+  for (let c of re_include) {
+    const lead = c.slice(0, c.length - 1);
+    if (isPartial(parts, lead)) {
+      // Check for partial matches (to ensure dirs get included)
+      if (partialPathMatch(parts, lead)) {
+        return true;
+      }
+    } else {
+      const re = new RegExp(re_join(c));
+      if (re.test(p)) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 function filterFunc(src, dest) {
   const relative = path.relative(srcDir, src);
-  const sub = inSubDir(src, srcDir);
-  return sub || exclude.indexOf(relative) === -1;
+  return pathOk(relative);
 }
 
-fse.copy(srcDir, dstDir, {filter: filterFunc});
-
-
-// TODO: Trim to same resources as notebook?
-/*
-# Trim mathjax
-mj = lambda *path: pjoin(components, 'MathJax', *path)
-static_data.extend([
-    mj('MathJax.js'),
-    mj('config', 'TeX-AMS-MML_HTMLorMML-full.js'),
-    mj('config', 'Safe.js'),
-])
-
-trees = []
-mj_out = mj('jax', 'output')
-
-if os.path.exists(mj_out):
-    for output in os.listdir(mj_out):
-        path = pjoin(mj_out, output)
-        static_data.append(pjoin(path, '*.js'))
-        autoload = pjoin(path, 'autoload')
-        if os.path.isdir(autoload):
-            trees.append(autoload)
-
-for tree in trees + [
-    mj('localization'), # limit to en?
-    mj('fonts', 'HTML-CSS', 'STIX-Web', 'woff'),
-    mj('extensions'),
-    mj('jax', 'input', 'TeX'),
-    mj('jax', 'output', 'HTML-CSS', 'fonts', 'STIX-Web'),
-    mj('jax', 'output', 'SVG', 'fonts', 'STIX-Web'),
-    mj('jax', 'element', 'mml'),
-]:
-    for parent, dirs, files in os.walk(tree):
-        for f in files:
-            static_data.append(pjoin(parent, f))
-*/
+fse.copy(srcDir, dstDir, { filter: filterFunc });
